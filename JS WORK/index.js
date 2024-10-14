f892834a3a56e405faa6b82f61b063c27b38976d
supriyapandey93 <supriyapade11@gmail.com>
2024-10-14 15:38:25 +0530

    js work folder added

---
+++ JS WORK/index.js
@@ -0,0 +1,78 @@
+/*console.log("wwelcome to js");
+const a=12;
+console.log(a);
+if(a>10){
+    //let b=23;
+    console.log("a inside condition",+a);
+    a=40;
+}
+console.log("a outside condition",+b);
+*/
+// let a=12;
+// let b="12";
+// // console.log(typeof a);
+// // let today=Date();
+// // console.log(today);
+
+// if(a===b){
+//     console.log("welcome");
+// }
+// else{
+//     console.log("hello");
+// }
+
+// let myname="Amit";
+// let college="ABES EC";
+// let result=`hi my name is ${myname} and Im doing engineering from ${college}`;
+// // console.log(result);
+// // let key="sname"
+// const  students=[{
+//     sname:"AMIT",
+//     course:"B.tech",
+//     college:"ABESEC"
+// },
+// {
+//     sname:"Ram",
+//     course:"MCA",
+//     college:"ABESEC"
+
+// }
+
+// ]
+// // console.log("Name: "+students.sname+ "Course: "+students.course);
+
+// // const {sname}=students;
+// // students[key]="Rahul";
+// // console.log(students);
+
+
+
+// // function selectLanguage(language){
+// //     let data;
+// //     if(language=="c"){
+// //     function cCompiler(){
+// //         return "cCompiler selected";
+// //         }
+// //         data=cCompiler();
+
+
+// //     }
+// //         if(language=="java"){
+           
+// //         function javaCompiler(){
+// //         return "JavaCompiler selected";
+        
+// //         }
+// //         data=javaCompiler();
+// //     }
+
+// //     return data;
+// // }
+
+// // let output=selectLanguage("c");
+// // console.log(output);
+
+console.log("hello world");
+let parent=document.getElementsByClassName("Parent");
+console.log(parent);
+parent[0].innerHTML="<h1> data has change </h1>";
